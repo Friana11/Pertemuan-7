22db89efd3733911c7307d2bf7086189fd7e7983
Friana11 <frianawg12@gmail.com>
2023-11-18 17:56:10 +0700

    Pesan commit Anda di sini

---
+++ LumenServiceApp/app/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class User extends Model
+{
+    use Authenticatable, Authorizable;
+    protected $fillable = [
+        'name', 'email', 'password'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+        
+}
